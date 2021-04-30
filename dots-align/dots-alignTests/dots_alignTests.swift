768d4ed152272541bbc70606e72225406f7b8efc
Jonathan Godbout <jgodbout@bathfitter.com>
2021-04-30 15:12:31 -0400

    SceneKit game template

---
+++ dots-align/dots-alignTests/dots_alignTests.swift
@@ -0,0 +1,33 @@
+//
+//  dots_alignTests.swift
+//  dots-alignTests
+//
+//  Created by Jonathan Godbout on 2021-04-30.
+//
+
+import XCTest
+@testable import dots_align
+
+class dots_alignTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
