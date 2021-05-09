b1221df3e7692b7631017f9e538badf5539e5ca1
Jonathan Godbout <jgodbout@bathfitter.com>
2021-05-09 00:04:58 -0400

    Renamed app to Align Dots, fixed scaleMode to .aspectFit instead of .aspectFill to avoid crop

--- dots-align/dots-align/GameViewController.swift
+++ dots-align/dots-align/GameViewController.swift
@@ -17,7 +17,7 @@ class GameViewController: UIViewController {
         if let view = self.view as! SKView? {
             let scene = GameScene(size: CGSize(width: self.view.bounds.width, height: self.view.bounds.height))
             
-            scene.scaleMode = .aspectFill
+            scene.scaleMode = .aspectFit
             
             view.presentScene(scene)
             view.ignoresSiblingOrder = true
