0ceaa9f3bb6eda740123e391b2d57cf98158f57b
Jonathan Godbout <jon.godbout@gmail.com>
2021-07-28 21:51:11 -0400

    No more using peak mechanism, using angle cumul with direction switch detection

--- dots-align/dots-align/CloudAlternate.swift
+++ dots-align/dots-align/CloudAlternate.swift
@@ -9,16 +9,15 @@ import Foundation
 import SpriteKit
 
 class CloudAlternate: Cloud {
-    let peakSpacingAngle = 0.45 * Scalar.pi // note: may become weird when to high, something breaks
-    var lastPeakPos = Vector3d(0, 0, 0)
-    var debugLastPeakDot: Dot?
+    let speed: Scalar = 1.5
+    
+    var skipDirection = false
+    var angleSign = 1.0
+    var angleCumul = 0.0
+    var direction = Vector3d(0, 1, 0)
+    var lastDirection = Vector3d(0, 1, 0)
     
     init(nbPoints: Int, scene: GameScene, color: UIColor, radius: CGFloat, dotRadius: CGFloat, addGuides: Bool = false) {
-        if Const.Debug.showCloudDebug {
-            self.debugLastPeakDot = Dot(scene: scene, color: UIColor.red, point3d: Const.Cloud.alignedOrientation, radius: 0.5 * dotRadius, sphereRadius: radius)
-            self.debugLastPeakDot?.node.setScale(1)
-        }
-        
         super.init(
             nbPoints: nbPoints,
             scene: scene,
@@ -29,40 +28,38 @@ class CloudAlternate: Cloud {
     }
     
     override func desalign(x: Scalar, y: Scalar) {
+        self.skipDirection = true
         super.desalign(x: x, y: y)
-        self.updatePeakPos() // init peak pos
-        self.updateDotScale()
+        self.skipDirection = false
     }
     
     override func rotate(quaternion: Quat) {
         super.rotate(quaternion: quaternion)
+        
+        if !self.skipDirection {
+            let crossVector = quaternion.angle > 0.0 ? Vector3d(0, 0, 1) : Vector3d(0, 0, -1)
+            let direction = simd_cross(quaternion.axis, crossVector)
+            
+            if simd_dot(direction, self.lastDirection) < 0.0 {
+                self.angleSign *= -1.0
+            }
+            
+            self.angleCumul += self.angleSign * quaternion.angle
+            
+            self.lastDirection = direction
+        }
+
         self.updateDotScale()
     }
     
-    private func updatePeakPos() {
-        self.lastPeakPos = self.orientation
-        
-        self.debugLastPeakDot?.point = self.lastPeakPos
-        self.debugLastPeakDot?.update()
-    }
-    
     private func updateDotScale() {
-        let dot = abs(simd_dot(self.orientation, self.lastPeakPos))
-        let angleBetweenVectors = dot < 1.0 ? acos(dot) : 0 // acos assumes both vectors were normalized
-        let vectorToPeakSpacingAngle = Scalar.pi * angleBetweenVectors / self.peakSpacingAngle
-        
-        let scaleA = 1.0 - abs(sin(vectorToPeakSpacingAngle))
-        let scaleB = 1.0 - abs(cos(vectorToPeakSpacingAngle))
+        let scaleA = 1.0 - abs(sin(self.speed * self.angleCumul))
+        let scaleB = 1.0 - abs(cos(self.speed * self.angleCumul))
         
         for i in 0..<self.dots.count {
             let scale = (i < self.dots.count / 2) ? scaleA : scaleB
-            self.dots[i].setRadius(radius: self.dots[i].baseRadius * CGFloat(scale * scale))
-            self.dots[i].node.alpha = CGFloat(scale)
-        }
-        
-        // Reset source position when peak reached.
-        if angleBetweenVectors >= self.peakSpacingAngle {
-            self.updatePeakPos()
+            self.dots[i].setRadius(radius: self.dots[i].baseRadius * CGFloat(scale))
+            self.dots[i].node.alpha = CGFloat(scale * 1.5)
         }
     }
 }
