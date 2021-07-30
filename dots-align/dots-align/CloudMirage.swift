35a9364781be30d26ce6d270986a3216f433b7db
Jonathan Godbout <jon.godbout@gmail.com>
2021-07-29 22:34:02 -0400

    Fine-tuned CloudMirage dots

--- dots-align/dots-align/CloudMirage.swift
+++ dots-align/dots-align/CloudMirage.swift
@@ -60,7 +60,7 @@ class CloudMirage: Cloud {
         let split = (halfCount % 2 == 0) ? halfCount : halfCount + 1 // adapt to even/odd
         
         let factor = 1.5 // scales before clipping; stays visible longer, full scale longer, lowers gap between 2 states
-        let power = 0.5 // flattens towards 0; more abrupt transition // can create overlap when lowering < 1.0
+        let power = 0.8 // flattens towards 0; more abrupt transition // can create overlap when lowering < 1.0
         
         for i in 0..<self.dots.count {
             let scale = (i < split) ? scaleA : scaleB
